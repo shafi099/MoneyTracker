5d59cfc6c2a78933cb8d464ce6dd29e599d956cc
Shafi Shaik <shaikshafieluru@gmail.com>
2023-05-12 20:25:09 +0530

    final code
    MERN App

---
+++ server/models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', userSchema);
