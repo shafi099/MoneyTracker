5b980addacde062ce8bdd86932d860d96872cab8
Jashanpreet singh <jashanmaan459@gmail.com>
2023-05-31 00:18:25 +0530

    new file:   Client/src/Cursor.jsx 	new file:   Client/src/cursor.css

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import HomeApp from './HomeApp';
-import LoginForm from './LoginForm';
-import Register from './Register';
+import HomeApp from "./HomeApp";
+import LoginForm from "./LoginForm";
+import Register from "./Register";
 import { Routes, Route } from "react-router-dom";
+import { Cursor } from "./Cursor";
 
 function App() {
-
   return (
     <Routes>
-   <Route path="/" element={<LoginForm />} />
-   <Route  path='/Register' element={<Register />} />
-   {/* <Route path="/" element={<Register />}/> */}
-   <Route path="/HomeApp" element={<HomeApp />} />
-</Routes>
+    <Cursor />
+      <Route path="/" element={<LoginForm />} />
+      <Route path="/Register" element={<Register />} />
+      {/* <Route path="/" element={<Register />}/> */}
+      <Route path="/HomeApp" element={<HomeApp />} />
+    </Routes>
+
+    
   );
 }
 
---
+++ Client/src/Cursor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './cursor.css'
+
+const Cursor = () => {
+  return (
+    <div className="cursor"></div>
+
+  )
+}
+
+export default Cursor
